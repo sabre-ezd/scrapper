4bde7c959767ab9cfce376359c13f380aa090ced
sabre <djsoundnr1@gmail.com>
2019-05-16 17:30:48 +0200

    parsing html, looking for mp3 done # TODO: saving them with filename as id of element

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 #!usr/bin/env python3
-from selenium import webdriver
+# from selenium import webdriver
 import bs4
 import os
+
 # launch url
 url = 'http://chatbot.admiralbulldog.live/playsound'
 os.makedirs("soundboard", exist_ok=True)
 
-# create a browser session with webdriver
-driver = webdriver.Chrome()
-driver.get(url)
-driver.implicitly_wait(30)
-html_page = driver.page_source
-with open("source.html", "w") as file:
-    file.write(html_page)
-driver.quit()
+# # create a browser session with webdriver
+# driver = webdriver.Chrome()
+# driver.get(url)
+# # wait for all elements to load
+# driver.implicitly_wait(30)
+# html_page = driver.page_source
+# with open("source.html", "w") as file:
+#     file.write(html_page)
+# driver.quit()
 
-sounds = bs4.BeautifulSoup(html_page, "lxml")
+sounds = bs4.BeautifulSoup(open('source.html'), "lxml")
 for link in sounds.find_all('audio'):
-    print(link)
+    print(link, type(link))
+    filename = "soundboard/" + link['id'] + ".mp3"
+    print(filename)
+    print(link['src'])
