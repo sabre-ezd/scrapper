1b53fc24f6faa0cb94ff52f179f022368891fe7b
sabre <djsoundnr1@gmail.com>
2019-05-16 14:19:45 +0200

    first steps

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import requests
-import string
+#!usr/bin/env python3
+from selenium import webdriver
 
+# launch url
 url = 'http://chatbot.admiralbulldog.live/playsound'
+
+# create a browser session with webdriver
+driver = webdriver.Chrome()
+driver.implicitly_wait(30)
+driver.get(url)
+
